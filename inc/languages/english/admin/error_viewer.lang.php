1caca09fe5d10cf276aff876c13193a720a80d6e
dragonexpert <arceus@teamdimensional.net>
2018-02-01 13:23:10 -0600

    Create error_viewer.lang.php

---
+++ inc/languages/english/admin/error_viewer.lang.php
@@ -0,0 +1,13 @@
+<?php
+$l['error_viewer_front_end'] = "Front End";
+$l['error_viewer_back_end'] = "Admin End";
+$l['error_viewer_error_log'] = "Error Log";
+$l['error_viewer_date'] = "Date";
+$l['error_viewer_time'] = "Time";
+$l['error_viewer_file'] = "File";
+$l['error_viewer_line'] = "Line";
+$l['error_viewer_type'] = "Type";
+$l['error_viewer_message'] = "Message";
+$l['error_viewer_errors_warnings'] = "Errors &amp; Warnings";
+$l['error_viewer_can_view'] = "Can view error log";
+$l['error_viewer_file_not_found'] = "Unable to find the error log file.  Check your server &amp; optimization settings.";
