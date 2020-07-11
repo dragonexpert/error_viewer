9ae072a3ef8e87609d39db81ec07ab8f0668b8f5
Sven <SvePu@users.noreply.github.com>
2020-07-11 18:57:37 +0200

    german translation

---
+++ inc/languages/deutsch_sie/admin/error_viewer.lang.php
@@ -0,0 +1,16 @@
+<?php
+$l['error_viewer_front_end'] = "Mitglieder-Bereich";
+$l['error_viewer_front_end_desc'] = "Hier sind Fehler gelistet, die im Mitgliederbereich des Forums aufgetreten sind.";
+$l['error_viewer_back_end'] = "Administrations-Bereich";
+$l['error_viewer_back_end_desc'] = "Hier sind Fehler gelistet, die im Administrationsbereich des Forums aufgetreten sind.";
+$l['error_viewer_error_log'] = "Fehlerprotokoll";
+$l['error_viewer_error_log_frontend'] = "Fehlerprotokoll Mitglieder-Bereich";
+$l['error_viewer_error_log_backend'] = "Fehlerprotokoll Administrations-Bereich";
+$l['error_viewer_date'] = "Datum / Zeit";
+$l['error_viewer_file'] = "Datei / Script";
+$l['error_viewer_line'] = "Linie";
+$l['error_viewer_type'] = "Typ";
+$l['error_viewer_message'] = "Nachricht";
+$l['error_viewer_errors_warnings'] = "Fehler &amp; Warnungen";
+$l['error_viewer_can_view'] = "Kann Fehlerprotokoll sehen";
+$l['error_viewer_file_not_found'] = "Die Fehlerprotokolldatei konnte nicht gefunden werden. Überprüfen Sie Ihren Server und Optimierungseinstellungen.";
