aab10f4a828b3c1a49908a93c22a8750bb60026a
Sven <SvePu@users.noreply.github.com>
2020-07-15 10:02:09 +0200

    Update error_viewer.php

--- UPLOAD/admin/modules/tools/error_viewer.php
+++ UPLOAD/admin/modules/tools/error_viewer.php
@@ -46,10 +46,12 @@ if(!$filecontents)
     exit;
 }
 
-$entries = explode("\n\n", $filecontents);
+preg_match_all('/\<error\>(.*?)\<\/error\>/is', $filecontents, $matches);
+
+$entries = $matches[0];
 $itemcount = count($entries);
 $last = $itemcount - 1;
-unset($entries[$last]);
+//unset($entries[$last]);
 if($mybb->input['page'])
 {
     $current_page = $mybb->get_input("page", MyBB::INPUT_INT);
@@ -85,33 +87,16 @@ $table->construct_header($lang->error_viewer_message);
 $table->construct_row();
 foreach($error_array as $entry)
 {
-    if(function_exists('debug_backtrace') && $mybb->version_code >= 1820)
-    {
-        $back_trace = "\t<back_trace>(.*)<\/back_trace>\n";
-    }
-    else
-    {
-        $back_trace = '';
-    }
-    preg_match_all('/\A<error>\n'
-    .'\t<dateline>([0-9]+)<\/dateline>\n'
-    .'\t<script>(.*)<\/script>\n'
-    .'\t<line>([0-9]+)<\/line>\n'
-    .'\t<type>([0-9]+)<\/type>\n'
-    .'\t<friendly_type>(.*)<\/friendly_type>\n'
-    .'\t<message>(.*)<\/message>\n'
-    .$back_trace
-    .'<\/error>(.*?)\Z/is', $entry, $string);
-
+    preg_match_all('/<dateline>([0-9]+)<\/dateline>|<script>(.*)<\/script>|<line>([0-9]+)<\/line>|<friendly_type>(.*)<\/friendly_type>|<message>(.*)<\/message>/is', $entry, $string);
     $date = my_date('relative', $string[1][0]);
-    $filename = $string[2][0];
-    $line = $string[3][0];
+    $filename = $string[2][1];
+    $line = $string[3][2];
     if(!$line)
     {
         $line = '-';
     }
-    $friendly_type = $string[5][0];
-    $message = nl2br($string[6][0]);
+    $friendly_type = $string[4][3];
+    $message = nl2br($string[5][4]);
 
     $table->construct_cell($date, array("class" => "align_center"));
     $table->construct_cell($filename);
