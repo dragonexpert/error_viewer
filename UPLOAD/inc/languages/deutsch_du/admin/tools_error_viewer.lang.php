c2b461dd943658ffcfdff18188aefd650a475fa5
Sven <SvePu@users.noreply.github.com>
2023-02-03 00:20:09 +0100

    update german lang files

--- UPLOAD/inc/languages/deutsch_du/admin/tools_error_viewer.lang.php
+++ UPLOAD/inc/languages/deutsch_du/admin/tools_error_viewer.lang.php
@@ -1,4 +1,5 @@
 <?php
+
 $l['error_viewer_front_end'] = "Mitglieder-Bereich";
 $l['error_viewer_front_end_desc'] = "Hier sind Fehler gelistet, die im Mitgliederbereich des Forums aufgetreten sind.";
 $l['error_viewer_back_end'] = "Administrations-Bereich";
@@ -15,3 +16,12 @@ $l['error_viewer_errors_warnings'] = "Fehler &amp; Warnungen";
 $l['error_viewer_can_view'] = "Kann Fehlerprotokoll sehen";
 $l['error_viewer_file_not_found'] = "Die Fehlerprotokolldatei konnte nicht gefunden werden. Überprüfe die Server und Optimierungseinstellungen.";
 $l['error_viewer_no_entries'] = "Die Fehlerprotokolldatei enthält keine Einträge.";
+
+$l['error_viewer_clear_frontend_log'] = "Leere Log Mitgliederbereich";
+$l['error_viewer_clear_backend_log'] = "Leere Log Administrationsbereich";
+
+$l['error_viewer_clear_frontend_log_confirm'] = "Möchtest Du wirklich alle Fehlereinträge des Mitgliederbereiches löschen? - Das kann nicht rückgängig gemacht werden!";
+$l['error_viewer_clear_backend_log_confirm'] = "Möchtest Du wirklich alle Fehlereinträge des Administrationsbereiches löschen? - Das kann nicht rückgängig gemacht werden!";
+
+$l['error_viewer_all_logs_cleared'] = "Fertig, alle Logs wurden gelöscht!";
+$l['error_viewer_clearing_failed'] = "Leider kann die Protokolldatei aufgrund eines unbekannten Problems nicht gelöscht werden!";
