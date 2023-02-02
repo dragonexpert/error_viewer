8a255e525f5ab882083c2cb38ab976dd3a88ed61
Sven <SvePu@users.noreply.github.com>
2023-02-02 21:49:04 +0100

    clear log button

--- UPLOAD/admin/modules/tools/error_viewer.php
+++ UPLOAD/admin/modules/tools/error_viewer.php
@@ -4,6 +4,9 @@ if (!defined("IN_MYBB"))
     die("Direct access not allowed.");
 }
 
+$error_file_frontend = MYBB_ROOT . "/" . $mybb->settings['errorloglocation'];
+$error_file_backend = MYBB_ROOT . "/" . $config['admin_dir'] . "/" . $mybb->settings['errorloglocation'];
+
 $sub_tabs = array(
     "frontend" => array(
         "title" => $lang->error_viewer_front_end,
@@ -17,128 +20,179 @@ $sub_tabs = array(
     )
 );
 
-$location = $mybb->get_input("location");
-$fileexists = false;
-if ($location == "backend")
+if ($mybb->input['action'] == "prune" && $mybb->request_method == "post")
 {
-    $page->add_breadcrumb_item($lang->error_viewer_error_log_backend, "index.php?module=tools-error_viewer&amp;location=backend");
-    $page->output_header($lang->error_viewer_error_log_backend);
-    $page->output_nav_tabs($sub_tabs, 'backend');
-    if (@file_exists(MYBB_ROOT . "/" . $config['admin_dir'] . "/" . $mybb->settings['errorloglocation']))
+    if (!empty($mybb->input['location']))
     {
-        $fileexists = true;
-        $filecontents = @file_get_contents(MYBB_ROOT . "/" . $config['admin_dir'] . "/" . $mybb->settings['errorloglocation']);
-    }
-}
-else
-{
-    $page->add_breadcrumb_item($lang->error_viewer_error_log_frontend, "index.php?module=tools-error_viewer");
-    $page->output_header($lang->error_viewer_error_log_frontend);
-    $page->output_nav_tabs($sub_tabs, 'frontend');
-    if (@file_exists(MYBB_ROOT . "/" . $mybb->settings['errorloglocation']))
-    {
-        $fileexists = true;
-        $filecontents = @file_get_contents(MYBB_ROOT . "/" . $mybb->settings['errorloglocation']);
-        $location = "frontend";
-    }
-}
-if (!isset($filecontents) && $fileexists !== true)
-{
-    $page->output_error($lang->error_viewer_file_not_found);
-    $page->output_footer();
-    exit;
-}
-
-$entries = explode("\n\n", $filecontents);
-$itemcount = count($entries);
-$last = $itemcount - 1;
-unset($entries[$last]);
-if (!empty($mybb->input['page']))
-{
-    $current_page = $mybb->get_input("page", MyBB::INPUT_INT);
-}
-else
-{
-    $current_page = 1;
-}
-if (!$current_page)
-{
-    $current_page = 1;
-}
-$pages = ceil($last / 50);
-if ($current_page > $pages)
-{
-    $current_page = $pages;
-}
-$start = $current_page * 50 - 50;
-$pagination = draw_admin_pagination($current_page, 50, $last, "index.php?module=tools-error_viewer&amp;location=" . $location);
-
-/* Flip the array so the newest errors are shown first since that is usually what admins want. */
-$entries = array_reverse($entries);
-$error_array = array_slice($entries, $start, 50);
-
-echo $pagination;
-
-$table = new TABLE;
-$table->construct_header($lang->error_viewer_date, array("class" => "align_center", 'width' => '10%'));
-$table->construct_header($lang->error_viewer_file, array('width' => '20%'));
-$table->construct_header($lang->error_viewer_line, array("class" => "align_center", 'width' => '5%'));
-$table->construct_header($lang->error_viewer_type, array("class" => "align_center", 'width' => '10%'));
-$table->construct_header($lang->error_viewer_message);
-$table->construct_row();
-if (!empty($error_array))
-{
-    foreach ($error_array as $entry)
-    {
-        $back_trace = '';
-        if (function_exists('debug_backtrace') && $mybb->version_code >= 1820)
+        switch ($mybb->input['location'])
         {
-            $back_trace = "\t<back_trace>(.*)<\/back_trace>\n";
+            case "frontend":
+                $file = $error_file_frontend;
+                break;
+            case "backend":
+                $file = $error_file_backend;
+                break;
         }
 
-        $string = array();
+        $ret = false;
 
-        preg_match_all('/\A<error>\n'
-            . '\t<dateline>([0-9]+)<\/dateline>\n'
-            . '\t<script>(.*)<\/script>\n'
-            . '\t<line>([0-9]+)<\/line>\n'
-            . '\t<type>([0-9]+)<\/type>\n'
-            . '\t<friendly_type>(.*)<\/friendly_type>\n'
-            . '\t<message>(.*)<\/message>\n'
-            . $back_trace
-            . '<\/error>(.*?)\Z/is', $entry, $string);
-
-        if (empty($string))
+        if (is_writeable($file))
         {
-            continue;
+            $ret = file_put_contents($file, '', LOCK_EX);
+        }
+
+        if ($ret !== false)
+        {
+            flash_message($lang->error_viewer_all_logs_cleared, 'success');
         }
         else
         {
-            $date = my_date('relative', (int)$string[1][0]);
-            $filename = htmlspecialchars_uni($string[2][0]);
-            $line = $string[3][0] != 0 ? $string[3][0] : '-';
-            $friendly_type = $string[5][0];
-            $message = nl2br($string[6][0]);
+            flash_message($lang->error_viewer_clearing_failed, 'error');
         }
 
-        $table->construct_cell($date, array("class" => "align_center"));
-        $table->construct_cell($filename);
-        $table->construct_cell($line, array("class" => "align_center"));
-        $table->construct_cell($friendly_type, array("class" => "align_center"));
-        $table->construct_cell($message);
-        $table->construct_row();
-
-        unset($string);
+        admin_redirect("index.php?module=tools-error_viewer&amp;location={$mybb->input['location']}");
+        unset($file);
     }
 }
-else
+
+if (!$mybb->input['action'])
 {
-    $table->construct_cell($lang->error_viewer_no_entries, array('colspan' => '5'));
+    $location = $mybb->get_input("location");
+    $fileexists = false;
+    if ($location == "backend")
+    {
+        $page->add_breadcrumb_item($lang->error_viewer_error_log_backend, "index.php?module=tools-error_viewer&amp;location=backend");
+        $page->output_header($lang->error_viewer_error_log_backend);
+        $page->output_nav_tabs($sub_tabs, 'backend');
+        if (@file_exists($error_file_backend))
+        {
+            $fileexists = true;
+            $filecontents = @file_get_contents($error_file_backend);
+        }
+    }
+    else
+    {
+        $page->add_breadcrumb_item($lang->error_viewer_error_log_frontend, "index.php?module=tools-error_viewer");
+        $page->output_header($lang->error_viewer_error_log_frontend);
+        $page->output_nav_tabs($sub_tabs, 'frontend');
+        if (@file_exists($error_file_frontend))
+        {
+            $fileexists = true;
+            $filecontents = @file_get_contents($error_file_frontend);
+            $location = "frontend";
+        }
+    }
+    if (!isset($filecontents) && $fileexists !== true)
+    {
+        $page->output_error($lang->error_viewer_file_not_found);
+        $page->output_footer();
+        exit;
+    }
+
+    $entries = explode("\n\n", $filecontents);
+    $itemcount = count($entries);
+    $last = $itemcount - 1;
+    unset($entries[$last]);
+    if (!empty($mybb->input['page']))
+    {
+        $current_page = $mybb->get_input("page", MyBB::INPUT_INT);
+    }
+    else
+    {
+        $current_page = 1;
+    }
+    if (!$current_page)
+    {
+        $current_page = 1;
+    }
+    $pages = ceil($last / 50);
+    if ($current_page > $pages)
+    {
+        $current_page = $pages;
+    }
+    $start = $current_page * 50 - 50;
+    $pagination = draw_admin_pagination($current_page, 50, $last, "index.php?module=tools-error_viewer&amp;location=" . $location);
+
+    /* Flip the array so the newest errors are shown first since that is usually what admins want. */
+    $entries = array_reverse($entries);
+    $error_array = array_slice($entries, $start, 50);
+
+    echo $pagination;
+
+    $table = new TABLE;
+    $table->construct_header($lang->error_viewer_date, array("class" => "align_center", 'width' => '10%'));
+    $table->construct_header($lang->error_viewer_file, array('width' => '20%'));
+    $table->construct_header($lang->error_viewer_line, array("class" => "align_center", 'width' => '5%'));
+    $table->construct_header($lang->error_viewer_type, array("class" => "align_center", 'width' => '10%'));
+    $table->construct_header($lang->error_viewer_message);
     $table->construct_row();
+    if (!empty($error_array))
+    {
+        foreach ($error_array as $entry)
+        {
+            $back_trace = '';
+            if (function_exists('debug_backtrace') && $mybb->version_code >= 1820)
+            {
+                $back_trace = "\t<back_trace>(.*)<\/back_trace>\n";
+            }
+
+            $string = array();
+
+            preg_match_all('/\A<error>\n'
+                . '\t<dateline>([0-9]+)<\/dateline>\n'
+                . '\t<script>(.*)<\/script>\n'
+                . '\t<line>([0-9]+)<\/line>\n'
+                . '\t<type>([0-9]+)<\/type>\n'
+                . '\t<friendly_type>(.*)<\/friendly_type>\n'
+                . '\t<message>(.*)<\/message>\n'
+                . $back_trace
+                . '<\/error>(.*?)\Z/is', $entry, $string);
+
+            if (empty($string))
+            {
+                continue;
+            }
+            else
+            {
+                $date = my_date('relative', (int)$string[1][0]);
+                $filename = htmlspecialchars_uni($string[2][0]);
+                $line = $string[3][0] != 0 ? $string[3][0] : '-';
+                $friendly_type = $string[5][0];
+                $message = nl2br($string[6][0]);
+            }
+
+            $table->construct_cell($date, array("class" => "align_center"));
+            $table->construct_cell($filename);
+            $table->construct_cell($line, array("class" => "align_center"));
+            $table->construct_cell($friendly_type, array("class" => "align_center"));
+            $table->construct_cell($message);
+            $table->construct_row();
+
+            unset($string);
+        }
+    }
+    else
+    {
+        $table->construct_cell($lang->error_viewer_no_entries, array('colspan' => '5'));
+        $table->construct_row();
+    }
+
+    $table->output($lang->error_viewer_errors_warnings);
+
+    echo $pagination;
+
+    if (!empty($error_array) && $fileexists !== false)
+    {
+        $error_viewer_clear_log = "error_viewer_clear_{$location}_log";
+        $error_viewer_clear_log_confirm = "error_viewer_clear_{$location}_log_confirm";
+
+
+        $form = new Form("index.php?module=tools-error_viewer&amp;action=prune&amp;location={$location}", "post");
+        $buttons = array();
+        $buttons[] = $form->generate_submit_button($lang->$error_viewer_clear_log, array('name' => 'clear', 'onclick' => "return confirm('{$lang->$error_viewer_clear_log_confirm}');"));
+        $form->output_submit_wrapper($buttons);
+        $form->end();
+    }
+
+    $page->output_footer();
 }
-
-$table->output($lang->error_viewer_errors_warnings);
-
-echo $pagination;
-
-$page->output_footer();
--- UPLOAD/inc/languages/english/admin/tools_error_viewer.lang.php
+++ UPLOAD/inc/languages/english/admin/tools_error_viewer.lang.php
@@ -1,4 +1,5 @@
 <?php
+
 $l['error_viewer_front_end'] = "Front End";
 $l['error_viewer_front_end_desc'] = "Lists errors on front page of forum.";
 $l['error_viewer_back_end'] = "Admin End";
@@ -15,3 +16,12 @@ $l['error_viewer_errors_warnings'] = "Errors &amp; Warnings";
 $l['error_viewer_can_view'] = "Can view error log";
 $l['error_viewer_file_not_found'] = "Unable to find the error log file.  Check your server &amp; optimization settings.";
 $l['error_viewer_no_entries'] = "The error log file contains no entries.";
+
+$l['error_viewer_clear_frontend_log'] = "Clear Front End Log";
+$l['error_viewer_clear_backend_log'] = "Clear Back End Log";
+
+$l['error_viewer_clear_frontend_log_confirm'] = "Do you really want to prune all frontend error entries? - This cannot be undone!";
+$l['error_viewer_clear_backend_log_confirm'] = "Do you really want to prune all backend error entries? - This cannot be undone!";
+
+$l['error_viewer_all_logs_cleared'] = "Done, all logs have been cleared!";
+$l['error_viewer_clearing_failed'] = "Sorry, can not clear the log file due an unknown issues!";
